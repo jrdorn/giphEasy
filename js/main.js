45ab19c32fe71b3ecb8a81eccf92159931a31871
Josh Dorn <jr_dorn@yahoo.com>
2020-07-05 23:21:49 -0700

    fixed hyperlinks

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
+
+
 $( document ).ready(function(){
 
-    $('#submit').on('click', function(){
 
-     
+    $('.quicksearch').on('click', function(){
 
-      var userInput = $('#search').val();
+      var userInput = this.id;
 
       var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=8bOT9SFOdzb5sATlyta8Qm4c1XvXI1LS&limit=9';
 
-
-   
       $.ajax({url: queryURL, method: 'GET'}).done(function(response){
-
 
         for (let step=0; step < 9; step++){
             var gifstring = '#gif' + step.toString();
             var giphyURL = response.data[step].images.fixed_height.url;
             $(gifstring).attr('src', giphyURL);
         }
-
       });
-      
-      
+      return false;
+    })
+
+
+
+    $('#submit').on('click', function(){
+
+      var userInput = $('#search').val();
+
+      var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=8bOT9SFOdzb5sATlyta8Qm4c1XvXI1LS&limit=9';
+
+      $.ajax({url: queryURL, method: 'GET'}).done(function(response){
+
+        for (let step=0; step < 9; step++){
+            var gifstring = '#gif' + step.toString();
+            var giphyURL = response.data[step].images.fixed_height.url;
+            $(gifstring).attr('src', giphyURL);
+        }
+      });
       return false;
     })
     
