6b88693ae53a51d2a2963b185ec17948f43c7494
Josh Dorn <jr_dorn@yahoo.com>
2020-07-07 05:08:52 -0700

    def functions to shorten main.js

--- js/main.js
+++ js/main.js
@@ -1,53 +1,34 @@
-
-
 $( document ).ready(function(){
 
-  myUrl = 'http://api.giphy.com/v1/gifs/trending?api_key=8bOT9SFOdzb5sATlyta8Qm4c1XvXI1LS&limit=9'
-  $.ajax({url: myUrl, method: 'GET'}).done(function(response){
+  function myFunc(funcUrl){
+    $.ajax({url: funcUrl, method: 'GET'}).done(function(response){
+      for (let step=0; step < 9; step++){
+          var gifstring = '#gif' + step.toString();
+          var giphyURL = response.data[step].images.fixed_height.url;
+          $(gifstring).attr('src', giphyURL);
+      }
+    });
+  }
+  
 
-    for (let step=0; step < 9; step++){
-        var gifstring = '#gif' + step.toString();
-        var giphyURL = response.data[step].images.fixed_height.url;
-        $(gifstring).attr('src', giphyURL);
-    }
+  var trendingURL = 'http://api.giphy.com/v1/gifs/trending?api_key=8bOT9SFOdzb5sATlyta8Qm4c1XvXI1LS&limit=9';
+  myFunc(trendingURL);
+
+
+  var giphyAPI = 'http://api.giphy.com/v1/gifs/search?q=';
+  var myKey = '&api_key=8bOT9SFOdzb5sATlyta8Qm4c1XvXI1LS&limit=9';
+
+  $('.quicksearch').on('click', function(){
+    var quickInput = this.id;
+    var quickURL = giphyAPI + quickInput + myKey;
+    myFunc(quickURL);
+  })
+
+  $('#submit').on('click', function(){
+    var searchInput = $('#search').val();
+    var searchURL = giphyAPI + searchInput + myKey;
+    myFunc(searchURL);
+    return false;
+  })
+
   });
-
-
-    $('.quicksearch').on('click', function(){
-
-      var userInput = this.id;
-
-      var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=8bOT9SFOdzb5sATlyta8Qm4c1XvXI1LS&limit=9';
-
-      $.ajax({url: queryURL, method: 'GET'}).done(function(response){
-
-        for (let step=0; step < 9; step++){
-            var gifstring = '#gif' + step.toString();
-            var giphyURL = response.data[step].images.fixed_height.url;
-            $(gifstring).attr('src', giphyURL);
-        }
-      });
-      return false;
-    })
-
-
-
-    $('#submit').on('click', function(){
-
-      var userInput = $('#search').val();
-
-      var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=8bOT9SFOdzb5sATlyta8Qm4c1XvXI1LS&limit=9';
-
-      $.ajax({url: queryURL, method: 'GET'}).done(function(response){
-
-        for (let step=0; step < 9; step++){
-            var gifstring = '#gif' + step.toString();
-            var giphyURL = response.data[step].images.fixed_height.url;
-            $(gifstring).attr('src', giphyURL);
-        }
-      });
-      return false;
-    })
-    
-  });
-
